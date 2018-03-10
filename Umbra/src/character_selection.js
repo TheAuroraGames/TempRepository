e87a9607fbbc85638525751b041b88d46cbf2ce8
Fumemji <34274681+Fumemji@users.noreply.github.com>
2018-03-10 17:16:13 -0500

    Character selection and Main Death
    Male character selection works and Robin can die and works

--- Umbra/src/character_selection.js
+++ Umbra/src/character_selection.js
@@ -6,10 +6,13 @@ var surface = canvas.getContext("2d");
 
 var uInt;
 
+canvas.addEventListener("mousemove", checkPos);
+canvas.addEventListener("mouseup", checkClick);
+
 var boy = new Image();
 var girl = new Image();
-boy.src = "../img/BOY.png";
-girl.src = "../img/GIRL.png";
+boy.src = "../img/Main.character.png";
+girl.src = "../img/Main.Character(female).png";
 
 
 
@@ -53,7 +56,29 @@ function render()
 		surface.fillText("Select Your Gender",300,90);
 		surface.fillStyle = "red";
 		
-		surface.drawImage(boy, -25,125);
-		surface.drawImage(girl, 590,125);
+		surface.drawImage(boy, -50,125);
+		surface.drawImage(girl, 565,125);
 }
+
+function checkPos (mouseEvent){
+	if(mouseEvent.pageX || mouseEvent.pageY == 0){
+		mouseX = mouseEvent.pageX - this.offsetLeft;
+		mouseY = mouseEvent.pageY - this.offsetTop;
+	}else if (mouseEvent.offsetX || mouseEvent.offsetY == 0){
+		mouseX = mouseEvent.offsetX;
+		mouseY = mouseEvent.offsetY;
+	}
+}
+function checkClick(mouseEvent){
+	if (mouseX > -50 && mouseX < 462 && mouseY > 125 && mouseY < 635){
+			loadNextLevel();
+			canvas.removeEventListener("mousemove", checkPos);
+			canvas.removeEventListener("mouseup", checkClick);
+		}
+	else
+		{
+			loadMenu();
+		}
+	}	
+
 
