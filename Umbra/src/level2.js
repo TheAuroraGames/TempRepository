be003906ffe3a9bc503b1fb97dc2079760273cde
TheLampost <34274706+TheLampost@users.noreply.github.com>
2018-04-03 15:05:36 -0400

    Level 2
    sprites addded to level two

--- Umbra/src/level2.js
+++ Umbra/src/level2.js
@@ -1,9 +1,9 @@
 var canvas= document.querySelector("canvas");
 canvas.width = 1024;
 canvas.height = 1024;
+// RobinHP
 
 var frameSpeed=33.34;
-
 
 var robinHP = {
 	// the parameters of the health bars.
@@ -23,58 +23,83 @@ var robinHPBorder = {
 	height: 30
 };
 
-var ClownHP = {
+// NoseHP
+
+var clownHP = {
 	x: 750,
 	y: 50,
 	width: 200,
 	height: 20
 };
 
-var ClownHPBorder = {
+var clownHPBorder = {
 	x: 745,
 	y: 45,
 	width: 210,
 	height: 30
 };
 
-var ClownHealth = 30;
-var ClownMaxHealth= 30;
-var ClownPercent = ClownHealth/ ClownMaxHealth;
-var ClownSpeed = 4;
+var clownhealth = 10;
+var clownmaxhealth=10;
+var clownpercent = clownhealth/ clownmaxhealth;
+var clownSpeed = 5;
 
 var surface = canvas.getContext("2d");
 //X and Y parameters
 var PlayerData;
-var ClownData;
+var clownData;
 //Image Variables
 var RobinWalk;
 var RobinJump;
 var RobinPunch;
-var ClownBack;
-var Clowny;
+var clownBack;
+var clownPunch;
+var clowny;
 var Robin;
-var ClownDeath;
+var clownDeath;
+var RobinDeath;
+var clownJump;
 
+var MoveTimer = 0;
 var Uptime = Date.now();
 
 
 
 var uInt;
 
+var AiTimer = setInterval(function (){
+	MoveTimer++;
+}, 500);
+
+
+
+
+
+// Speed
+var clownJumpSpeed = 50;
 var RobinSpeed = 15	;
 
 var leftPressed = false;
 var rightPressed = false;
 var upPressed = false;
 var punchPressed = false;
+//var downPressed = false;
 
-var ClownDying = false; 
-var ClownDead = false;
+var RobinDying = false;
+var RobinDead = false;
+var clownJumpState = false;
+var clownPunchState = false;
+var clownDying = false; 
+var clownDead = false;
+var clownisJumping = false;
 var isJumping = false;
 var RobinJumpData;
 var RobinAnimData;
 var RobinWalkData;
-var ClownDeathData;
+var RobinDeathData;
+var clownDeathData;
+var clownPunchData;
+var clownJumpData;
 //Keyboard Listeners
 window.addEventListener("keydown", onKeyDown);
 window.addEventListener("keyup", onKeyUp);
@@ -85,27 +110,48 @@ createBackground();
 createRobinPunch();
 createRobinJump();
 createRobinWalk();
-createClownDeath();
+createClowndeath();
+createClownPunch();
+createClownJump();
+createRobinDeath();
+
 
 function loadNextLevel()
 {
-	window.location.href = 'level3.html';
+	window.location.href = 'Segment5.html';
 }
 
+function GameOver()
+{
+	window.location.href = 'GameOver.html';
+}
+
+
 //Checking collision function the parameters of this function are given input in the update function.
-function checkCollision(player1animdata,player1data,player2data,targetframe)
+function checkCollision(player1animdata,player1data,player2data,targetframe,isRight=true)
 {	
 //if the punching animation reaches the target frame 1 part of the collision is achieved.
 	if(Math.floor(player1animdata.currentFrame)== targetframe){
 	
+		var player1Edge ;
 		//checks whats the players right edge is.
-		var player1Edge = player1data.x + player1data.width;
-		
-		// this checks if players right edge hits the enemy.
+		if (isRight==true){player1Edge = player1data.x + player1data.width; 
+		// this checks if players  edge hits the enemy.
 		if (player1Edge>= player2data.x&& player1Edge<=(player2data.x + player2data.width)){
 			
 			return true;
 		}
+		}
+		else {
+			player1Edge = player1data.x; 
+			
+		// this checks if players  edge hits the enemy.
+		if (player1Edge>= player2data.x&& player1Edge<=(player2data.x + player2data.width)){
+			
+			return true;
+		}
+		}
+		
 		return false;
 		
 	}
@@ -123,7 +169,36 @@ function update()
 	
 	if (typeof dt == 'undefined') dt=1;
 	
-	Uptime = now;
+	Uptime = now;	
+	if(clownDying&&clownDead==false)
+	{
+		clownDead= Animate(clownDeathData,dt);
+		
+	}
+	else if (clownPunchState&&clownDead==false){
+		clownPunchState= ! Animate(clownPunchData,dt);
+		if(checkCollision(clownPunchData,clownData,PlayerData,10,false))
+		{
+			health --;
+			percent = health/maxhealth;
+			if(percent <= 0){
+				RobinDying = true;
+				percent = 0;
+			}
+		}
+	}else if (clownJumpState&&clownDead==false){
+	clownJumpState= ! Animate(clownJumpData,dt);
+	}
+	
+	if(RobinDying&&RobinDead==false)
+	{
+		RobinDead= Animate(RobinDeathData,dt);
+		
+	}
+	if(RobinDead == true)
+	{
+		GameOver();
+	}
 	
 // if we are in jumping state then it will animation the jumping.
 	if (isJumping)
@@ -133,26 +208,22 @@ function update()
 	{
 		Animate(RobinAnimData,dt);
 		//checking collision with these 3 parameters
-		if(checkCollision(RobinAnimData,PlayerData,ClownData,6))
+		if(checkCollision(RobinAnimData,PlayerData,clownData,6))
 		{
 		// if theses parameters pass then we do DMG
-				ClownHealth --;
-				ClownPercent = ClownHealth/ClownMaxHealth;
-				if (ClownPercent <= 0){
-					ClownDying = true;
+				clownhealth --;
+				clownpercent = clownhealth/clownmaxhealth;
+				if (clownpercent <= 0){
+					clownDying = true;
 					
 					//Fill in later with proper level ending.
-					ClownPercent = 0;
+					clownpercent = 0;
 
 				}
 				
 		}
 	}
-	if(ClownDying&&ClownDead==false)
-	{
-		ClownDead= Animate(ClownDeathData,dt);
-		
-	}
+
 
 //if the left or right button is pressed then the walking animation is pressed.
 	if(leftPressed || rightPressed)
@@ -160,7 +231,8 @@ function update()
 		Animate(RobinWalkData,dt);
 	}
 	moverobin(dt);
-	moveClownAI(dt);
+	moveclownAI(dt);
+	clownAction(dt);
 	render();
 	
 	
@@ -234,13 +306,34 @@ function createRobinPunch()
 	
 	RobinAnimData.PunchSound.src = "../audio/punch.wav";
 }
+function createRobinDeath()
+{
+	RobinDeath = new Image();
+	RobinDeath.src = "../img/Robin_Death.png";
+	RobinDeathData = {
+	row :3,
+	col :3,
+	MaxFrame :8,
+	x:0,
+	y:0,
+	width:512,
+	height:512,
+	currentFrame:0,
+	looping: false,
+	// Audio is played every time the player jumps
+	// DeadSound: new Audio()
+	};
+	
+// add sound
+	
+}
+
 
 function createBackground()
 {
-	ClownBack = new Image();
-	ClownBack.src = "../img/ClownBackground.png";
+	clownBack = new Image();
+	clownBack.src = "../img/clownBackground.png";
 }
-
 function createHero()
 {
 	Robin = new Image();
@@ -256,45 +349,137 @@ function createHero()
 	
 }
 
-function createClown()
+function createClownPunch()
 {
-	Clowny = new Image();
-	Clowny.src = "../img/Clown.png";
-	ClownData = {};
-	ClownData.x = 750;
-	ClownData.y = 325;
-	ClownData.width= 250;
-	ClownData.height=450;
+	clownPunch = new Image();
+	clownPunch.src = "../img/Clown_Punch.png";
+	clownPunchData ={
+	row:4,
+	col:3,
+	MaxFrame:11,
+	x:0,
+	y:0,
+	width:512,
+	height:512,
+	currentFrame:0,
+	looping: false,
+	clownPunchSound: new Audio()
+	};
+	clownPunchData.clownPunchSound.src="../audio/punch.wav";
+}
+function createClownJump()
+{
+	clownJump = new Image();
+	clownJump.src = "../img/Clown_Jump.png";
+	clownJumpData={
+	row :4,
+	col :3,
+	MaxFrame :10,
+	x:0,
+	y:0,
+	width:512,
+	height:512,
+	currentFrame:0,
+	looping: true,
+	// Audio is played every time the player jumps
+	clownJumpSound: new Audio()
+	};
+	
+	clownJumpData.clownJumpSound.src = "../audio/jump.wav";
 }
 
-function moveClownAI(deltaTime)
+function createClown()
 {
-	if (ClownData.x != PlayerData.x && ClownData.y != PlayerData.y){
+	clowny = new Image();
+	clowny.src = "../img/Clown.png";
+	clownData = {};
+	clownData.x = 750;
+	clownData.y = 335;
+	clownData.width= 250;
+	clownData.height=450;
+	clownData.gravity = 0.05;
+	clownData.gravitySpeed = 0.00;
+	
+}
+
+function clownAction(deltaTime)
+{
+if(clownDead||clownDying){
+	return;
+}
+	if (MoveTimer == 3 && clownPunchState == false)
+	{
+		clownPunchState = true;
+		clownPunchData.clownPunchSound.play();
+		
+	
+	}
+	else if(MoveTimer == 6 && clownJumpState == false)
+	{
+		clownJumpState = true;
+		clownisJumping=true;
+		clownJumpData.clownJumpSound.play();
+	
+	}
+	
+	if (MoveTimer>6)MoveTimer=0; 
+	
+	
+		
+}
+
+function moveclownAI(deltaTime)
+{
+	if (clownJumpState){
+		if (clownisJumping == true)
+	{
+		// formula used for gravity.
+		clownData.y -= (clownJumpSpeed + clownData.gravity)*deltaTime;
+		//if Nosedata.y reaches 165 come back down.
+			if (clownData.y < 165)
+			{
+				clownisJumping = false;
+			}
+	}
+	else
+	{
+		
+		//formula used for gravity.
+			clownData.y += (clownJumpSpeed + clownData.gravity)*deltaTime;
+	}
+	}
+	else if (clownData.x != PlayerData.x && clownData.y != PlayerData.y){
 		if (Math.floor(Math.random() * 2) == 1){
-			if (ClownData.x < PlayerData.x) ClownData.x = ClownData.x + (ClownSpeed*deltaTime);
-			else if  (ClownData.x > PlayerData.x) ClownData.x = ClownData.x - (ClownSpeed*deltaTime);
+			if (clownData.x < PlayerData.x) clownData.x = clownData.x + (clownSpeed*deltaTime);
+			else if  (clownData.x > PlayerData.x) clownData.x = clownData.x - (clownSpeed*deltaTime);
 		}
 	}
 	else {
-		if (ClownData.x < PlayerData.x) ClownData.x = ClownData.x + (ClownSpeed*deltaTime);
-		else if (ClownData.x > PlayerData.x) ClownData.x =  ClownData.x - (ClownSpeed*deltaTime);
+		if (clownData.x < PlayerData.x) clownData.x = clownData.x + (clownSpeed*deltaTime);
+		else if (clownData.x > PlayerData.x) clownData.x =  clownData.x - (clownSpeed*deltaTime);
 	}
-	if (ClownData.x<0) ClownData.x =0;
-	if (ClownData.x>800) ClownData.x= 800;
+	if (clownData.x<0) clownData.x =0;
+	if (clownData.x>800) clownData.x= 800;
 	
-	if (ClownData.x==NaN) ClownData.x = 750;
+	if (clownData.x==NaN) clownData.x = 750;
+	if (clownData.y<165) clownData.y = 165;
+	if (clownData.y>335) 
+	{
+		clownData.y= 335;
+		clownJumpState = false;
+	}
 	
 	
 }
 
-function createClownDeath()
+function createClowndeath()
 {
-	ClownDeath = new Image();
-	ClownDeath.src = "../img/Clown_Death.png"; 
-	ClownDeathData = {
-	row :5,
-	col :4,
-	MaxFrame :20,
+	clownDeath = new Image();
+	clownDeath.src = "../img/Clown_Death.png";
+	clownDeathData = {
+	row :3,
+	col :3,
+	MaxFrame :8,
 	x:0,
 	y:0,
 	width:512,
@@ -308,6 +493,7 @@ function createClownDeath()
 // add sound
 	
 }
+
 
 function moverobin(deltaTime)
 {
@@ -346,12 +532,18 @@ function moverobin(deltaTime)
 	
 }
 
+
+
+
+
+
+
 function render()
 {
 	//Clears Canvas
 	surface.clearRect(0,0,canvas.width,canvas.height);
 	//draws background
-	surface.drawImage(ClownBack,0,0,1024,768);
+	surface.drawImage(clownBack,0,0,1024,768);
 	//creates the health border for player
 	surface.fillStyle = "black";
     surface.fillRect(robinHPBorder.x, robinHPBorder.y, robinHPBorder.width, robinHPBorder.height);
@@ -363,15 +555,20 @@ function render()
 	surface.fillRect(robinHP.x, robinHP.y, robinHP.width * percent, robinHP.height);
 	// creates the health border for vampire.
 	surface.fillStyle = "black";
-    surface.fillRect(ClownHPBorder.x, ClownHPBorder.y, ClownHPBorder.width, ClownHPBorder.height);
+    surface.fillRect(clownHPBorder.x, clownHPBorder.y, clownHPBorder.width, clownHPBorder.height);
 	// creates the rectangle behind the health bar to give illusion of damage taken.
 	surface.fillStyle = "red";
-    surface.fillRect(ClownHP.x, ClownHP.y, ClownHP.width, ClownHP.height);
+    surface.fillRect(clownHP.x, clownHP.y, clownHP.width, clownHP.height);
 	// creates the health bar.
 	surface.fillStyle= "green";
-	surface.fillRect(ClownHP.x, ClownHP.y, ClownHP.width * ClownPercent, ClownHP.height);
+	surface.fillRect(clownHP.x, clownHP.y, clownHP.width * clownpercent, clownHP.height);
 	//if the punch button is pressed draws the punching animation.
-	if(punchPressed){
+	if (RobinDead == false){
+	if(RobinDying)
+	{
+		surface.drawImage(RobinDeath,RobinDeathData.x, RobinDeathData.y,512,512, PlayerData.x, PlayerData.y,PlayerData.width,PlayerData.height);
+	}
+	else if(punchPressed){
 		surface.drawImage(RobinPunch,RobinAnimData.x,RobinAnimData.y,512,512,PlayerData.x,PlayerData.y,PlayerData.width,PlayerData.height);
 	// if in jumping state and punching isnt pressed draws the jumping animation.
 	}else if(isJumping){
@@ -387,16 +584,26 @@ function render()
 	
 	else{
 		surface.drawImage(Robin,PlayerData.x,PlayerData.y,PlayerData.width,PlayerData.height);
+	}
 	}	
-	if (ClownDead == false)
+	
+	if (clownDead == false)
 	{
-		if(ClownDying)
+		if(clownDying)
 		{
-			surface.drawImage(ClownDeath, ClownDeathData.x, ClownDeathData.y, 512,512, ClownData.x, ClownData.y, ClownData.width, ClownData.height);
+			surface.drawImage(clownDeath, clownDeathData.x, clownDeathData.y, 512,512, clownData.x, clownData.y, clownData.width, clownData.height);
+		}
+		else if(clownPunchState == true)
+		{
+			surface.drawImage(clownPunch,clownPunchData.x,clownPunchData.y,512,512,clownData.x,clownData.y,clownData.width, clownData.height);
+		}
+		else if(clownJumpState == true)
+		{
+			surface.drawImage(clownJump,clownJumpData.x,clownJumpData.y,512,512,clownData.x,clownData.y,clownData.width,clownData.height);
 		}
 		else
 		{
-			surface.drawImage(Clowny,ClownData.x,ClownData.y,ClownData.width,ClownData.height);
+			surface.drawImage(clowny,clownData.x,clownData.y,clownData.width,clownData.height);
 		}
 	}
 	
@@ -421,8 +628,9 @@ function Animate(objToAnimate,deltaTime)
 	}
 	else //dont loop
 	{
-		if (Math.floor(objToAnimate.currentFrame) >= objToAnimate.MaxFrame-1)
+		if (Math.floor(objToAnimate.currentFrame)>=objToAnimate.MaxFrame-1)
 		{
+			objToAnimate.currentFrame=0;
 			return true;
 		}	
 		else 
@@ -458,8 +666,8 @@ function onKeyDown(event)
 			punchPressed =true;
 			RobinAnimData.PunchSound.play();
 			break;
-		case 32:
-		if(ClownDead == true)
+		case 32://SpaceBar
+		if(clownDead == true)
 			loadNextLevel();
 			break;
 		
@@ -470,7 +678,6 @@ function onKeyDown(event)
 			
 	} 
 }
-
 function onKeyUp(event)
 {
 	switch (event.keyCode)
